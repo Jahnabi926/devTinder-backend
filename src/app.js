3f7d5f0285c901959fa585fafa2028b0bff46855
Jahnabi Sarma <sarmajahnabi8@gmail.com>
2026-06-25 14:01:39 +0530

    Exploring get, post & delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,32 @@ const express = require("express"); // importing the module express from node mo
 
 const app = express(); // calling express
 
+// Order of the below routes matter
 // Specific routes go first
-app.use("/test", (req, res) => res.send("Hello test request handler"));
 
-app.use("/hello", (req, res) => res.send("Hello from request handler hello"));
+app.use("/user", (req, res) => {
+  res.send("Overridden All http methods to /user");
+});
 
-// Generic catch-all prefix goes last or use app.get for all
-app.use("/", (req, res) => res.send("Hello from the Server")); // request handler. Server responding to incoming requests from browser
+// this will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Jahnabi", lastName: "Sarma" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data saved to database successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted from database successfully");
+});
+
+// This will match all the HTTP method API calls to "/"
+app.use("/", (req, res) =>
+  res.send(
+    "Overridden All http methods to /user except above written methods get, post & delete",
+  ),
+); // request handler. Server responding to incoming requests from browser
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
