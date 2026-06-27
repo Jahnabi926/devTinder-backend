b3b5c82bd9b914a2438f7e6d6e611d9193253374
Jahnabi Sarma <sarmajahnabi8@gmail.com>
2026-06-27 23:42:55 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -7,9 +7,10 @@ const app = express(); // calling express
 
 app.use(express.json()); // express.json is a middleware to convert the json data of request body to js object and send to the server to add it to the database
 
+// create users in the database
 app.post("/signup", async (req, res) => {
   // creating a new user instance of the User model
-  const user = new User(req.body);
+  const user = new User(req.body); // Creates a new user object in memory (like a JS object)
 
   try {
     await user.save(); // saves the data to the database
@@ -17,6 +18,39 @@ app.post("/signup", async (req, res) => {
   } catch (error) {
     res.status(400).send("Error saving the user:" + error.message);
   }
+});
+
+// get user by emailId -- read (from database)
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = await User.find({ emailId: "sarma5@gmail.com" }); // goes to DB, finds matching docs, returns them as an array
+    if (userEmail.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(userEmail);
+    }
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const feed = await User.find({}); // find all documents
+    if (feed.length === 0) {
+      res.status(404).send("Users not found");
+    } else {
+      res.send(feed);
+    }
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// ⬇️ catches everything that didn't match above
+app.use((req, res) => {
+  res.status(404).send("Route not found !");
 });
 
 connectDB()
