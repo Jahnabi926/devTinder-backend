c90a37ce94552411a991d36ae8dece729eda221c
Jahnabi Sarma <sarmajahnabi8@gmail.com>
2026-07-08 23:05:22 +0530

    Feed API &  Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -47,6 +48,49 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
 
     res.json({ data });
+  } catch (error) {
+    res.status(400).send("ERROR : ", error.message);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    // User should see all the user cards except
+    // 1. his own card
+    // 2. his connections
+    // 3. ignored people
+    // 4. who they alraedy sent the connection request
+
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    // query - Find all connection requests of the loggedInUser (sent + received)
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    // db call - to find all the users who are not present in the array hideUsersFromFeed and their id is not in the loggedinuser id
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
   } catch (error) {
     res.status(400).send("ERROR : ", error.message);
   }
