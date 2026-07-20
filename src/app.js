16b7a5e07357db073a61a1228b09b917f8076629
Jahnabi Sarma <sarmajahnabi8@gmail.com>
2026-07-20 17:10:10 +0530

    Web Sockets & socket.io

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express(); // calling express
 require("dotenv").config();
 require("./utils/cronjob");
+const http = require("http");
 
 app.use(
   cors({
@@ -20,12 +21,16 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 const paymentRouter = require("./routes/payment");
+const initializeSocket = require("./utils/socket");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use("/", paymentRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 // ⬇️ catches everything that didn't match above
 app.use((req, res) => {
@@ -35,7 +40,7 @@ app.use((req, res) => {
 connectDB()
   .then(() => {
     console.log("Database Connection established...");
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("Server is successfully listening on port 7777...");
     }); // created a web server which is listening on port 7777
   })
